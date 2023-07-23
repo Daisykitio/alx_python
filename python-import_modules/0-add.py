9dc4566cd50c441ae4e0b93949f714d9c16dffb2
Daisykitio <daisycheptoek6788@gmail.com>
2023-07-23 13:09:12 -0700

    Import a simple function from a simple file

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+
+"""Import a simple function"""
+
 from add_0 import add
 
 a = 1
@@ -6,3 +9,5 @@ b = 2
 result = add(a, b)
 
 print("{} + {} = {}".format(a, b, result))
+if __name__ == "__main__":
+    pass
