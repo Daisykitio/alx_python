3c6bce23eb84379556695edc80b74f05761f6062
Daisykitio <daisycheptoek6788@gmail.com>
2023-07-24 13:52:46 -0700

    1. Lists of lists = Matrix

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -2,22 +2,11 @@
 
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for i in range(len(row)):
-            # Print each element of the row using str.format()
-            # with end=' ' to print a space instead of a newline after each element
-            print("{:d}".format(row[i]), end=' ')
-
-        # After printing each row, add a newline to start the next row
-        print()
-
-# Test the function with the provided example
-if __name__ == "__main__":
-    matrix = [
-        [1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 9]
-    ]
-
-    print_matrix_integer(matrix)
-    print("--")
-    print_matrix_integer()
+        for i, num in enumerate(row):
+            # Use str.format() to print integers without casting them to strings
+            if i == len(row) - 1:
+                # For the last element in the row, add a newline character after the number
+                print("{:d}".format(num))
+            else:
+                # For other elements, add a space after the number
+                print("{:d}".format(num), end=" ")
