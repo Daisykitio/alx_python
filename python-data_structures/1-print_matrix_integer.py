7b148ba4d1c5ea129b2e22ee14b4b1080bb40093
Daisykitio <daisycheptoek6788@gmail.com>
2023-07-26 13:26:26 -0700

    1. Lists of lists = Matrix

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -9,4 +9,3 @@ def print_matrix_integer(matrix=[[]]):
             # Use str.format() to print integers without casting them to strings
             end_char = "\n" if i == len(row) - 1 else " "
             print("{:d}".format(num), end=end_char)
-
