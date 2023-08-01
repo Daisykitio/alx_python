84758393f0fab7ac4833ca9371e06fdd35161b5a
Daisykitio <daisycheptoek6788@gmail.com>
2023-08-01 13:34:05 -0700

    Size validation

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,42 +1,48 @@
+"""
+Module 1-square
+==============
+
+This module defines the Square class that represents a square with a private
+attribute "size". The size attribute is essential for a square as it is used in
+computing the area and other operations. Keeping it private ensures proper
+control over its value and type.
+
+Example:
+    # Create a square with size 5
+    square = Square(5)
+"""
 class Square:
     def __init__(self, size=0):
+        """
+        Initializes a Square instance with an optional size.
+
+        Args:
+            size (int, optional): The size of the square. Defaults to 0.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
+
         if size < 0:
             raise ValueError("size must be >= 0")
+
         self.__size = size
 
 # Testing the Square class
 if __name__ == "__main__":
-    my_square_1 = Square(3)
-    print(type(my_square_1))
-    print(my_square_1.__dict__)
-
-    my_square_2 = Square()
-    print(type(my_square_2))
-    print(my_square_2.__dict__)
-
     try:
-        print(my_square_1.size)
-    except Exception as e:
-        print(e)
+        square1 = Square(5)
+        print("square1 size:", square1._Square__size)  # Output: 5
 
-    try:
-        print(my_square_1.__size)
-    except Exception as e:
-        print(e)
+        square2 = Square()  # Default size (0)
+        print("square2 size:", square2._Square__size)  # Output: 0
 
-    try:
-        my_square_3 = Square("3")
-        print(type(my_square_3))
-        print(my_square_3.__dict__)
+        # Testing with invalid sizes
+        square3 = Square("invalid")  # Raises TypeError
+        square4 = Square(-2)  # Raises ValueError
     except Exception as e:
-        print(e)
-
-    try:
-        my_square_4 = Square(-89)
-        print(type(my_square_4))
-        print(my_square_4.__dict__)
-    except Exception as e:
-        print(e)
+        print("Error:", e)
 
