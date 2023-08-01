043f4c2d44b5175d33c02a9e91799fe928375b19
Daisykitio <daisycheptoek6788@gmail.com>
2023-08-01 12:53:54 -0700

    Square with size

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,18 +1,13 @@
 class Square:
-    """
-    Represents a square.
-
-    Attributes:
-        __size (int): The size of the square.
-
-    """
-
     def __init__(self, size):
-        """
-        Initializes a Square instance.
-
-        Args:
-            size (int): The size of the square.
-
-        """
         self.__size = size
+
+# Testing the Square class
+if __name__ == "__main__":
+    square = Square(5)
+    # Accessing the private attribute directly will raise an AttributeError
+    # print(square.__size)  # This would raise an AttributeError
+
+    # Accessing the private attribute using a "name mangling" syntax (_ClassName__attribute)
+    # This is not recommended as it violates the encapsulation principle
+    print(square._Square__size)
