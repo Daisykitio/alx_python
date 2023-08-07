ff71404bd61b7fab689d2397f594dd0a88c808de
Daisykitio <daisycheptoek6788@gmail.com>
2023-08-07 14:07:59 -0700

    Update #1

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -2,45 +2,31 @@
 models.rectangle
 ================
 
-This module contains the definition of the Rectangle class, which inherits from the Base"""
+This module contains the definition of the Rectangle class, which inherits from the Base class.
+"""
+
 from models.base import Base
 
-class Square(Rectangle):
-    """The Square class represents a square."""
+class Rectangle(Base):
+    """The Rectangle class represents a rectangle."""
 
-    def __init__(self, size, x=0, y=0, id=None):
-        """Constructor for the Square class.
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Constructor for the Rectangle class.
 
         Args:
-            size (int): Size of the square (same as width and height).
-            x (int, optional): x-coordinate of the square's position. Defaults to 0.
-            y (int, optional): y-coordinate of the square's position. Defaults to 0.
-            id (int, optional): ID of the square. Defaults to None.
+            width (int): Width of the rectangle.
+            height (int): Height of the rectangle.
+            x (int, optional): x-coordinate of the rectangle's position. Defaults to 0.
+            y (int, optional): y-coordinate of the rectangle's position. Defaults to 0.
+            id (int, optional): ID of the rectangle. Defaults to None.
         """
-        super().__init__(size, size, x, y, id)
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
-    # The __str__ method is inherited from Rectangle
-
-    def update(self, *args, **kwargs):
-        """Update attributes with the provided arguments."""
-        if args:
-            attributes = ['id', 'size', 'x', 'y']
-            for i, arg in enumerate(args):
-                setattr(self, attributes[i], arg)
-        elif kwargs:
-            for key, value in kwargs.items():
-                setattr(self, key, value)
-    
-    @property
-    def size(self):
-        """Getter for the size attribute (same as width and height)."""
-        return self.width
-    
-    @size.setter
-    def size(self, value):
-        """Setter for the size attribute  (same as width and height)."""
-        self.width = value
-        self.height = value
+    # Existing properties and setters...
 
     def area(self):
         """Calculate and return the area of the rectangle."""
