26c6c6426309f173dc14580c25cdbcfafce4e813
Daisykitio <daisycheptoek6788@gmail.com>
2023-08-07 12:18:56 -0700

    Area first

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -4,7 +4,6 @@ models.rectangle
 
 This module contains the definition of the Rectangle class, which inherits from the Base class.
 """
-
 from models.base import Base
 
 class Rectangle(Base):
@@ -26,59 +25,9 @@ class Rectangle(Base):
         self.x = x
         self.y = y
 
-    @property
-    def width(self):
-        """Getter for the width attribute."""
-        return self.__width
+    # Existing properties and setters...
 
-    @width.setter
-    def width(self, value):
-        """Setter for the width attribute."""
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value <= 0:
-            raise ValueError("width must be > 0")
-        self.__width = value
-
-    @property
-    def height(self):
-        """Getter for the height attribute."""
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        """Setter for the height attribute."""
-        if not isinstance(value, int):
-            raise TypeError("height must be an integer")
-        if value <= 0:
-            raise ValueError("height must be > 0")
-        self.__height = value
-
-    @property
-    def x(self):
-        """Getter for the x attribute."""
-        return self.__x
-
-    @x.setter
-    def x(self, value):
-        """Setter for the x attribute."""
-        if not isinstance(value, int):
-            raise TypeError("x must be an integer")
-        if value < 0:
-            raise ValueError("x must be >= 0")
-        self.__x = value
-
-    @property
-    def y(self):
-        """Getter for the y attribute."""
-        return self.__y
-
-    @y.setter
-    def y(self, value):
-        """Setter for the y attribute."""
-        if not isinstance(value, int):
-            raise TypeError("y must be an integer")
-        if value < 0:
-            raise ValueError("y must be >= 0")
-        self.__y = value
-
+    def area(self):
+        """Calculate and return the area of the rectangle."""
+        return self.width * self.height
+    
