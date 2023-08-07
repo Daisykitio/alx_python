c2bfd61dac00e421daece838143e9a11102a0b47
Daisykitio <daisycheptoek6788@gmail.com>
2023-08-07 11:44:58 -0700

    Validate attributes

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -4,6 +4,7 @@ from models.base import Base
 
 class Rectangle(Base):
 
+     """The Rectangle class represents a rectangle."""
     def __init__(self, width, height, x=0, y=0, id=None):
         """Constructor for the Rectangle class.
 
