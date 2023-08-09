f3d7ad361edb5f60ec46548a6b67f1260225c1cf
Daisykitio <daisycheptoek6788@gmail.com>
2023-08-09 13:59:15 -0700

    What's my status? #1

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 import requests
 
-url = 'https://alu-intranet.hbtn.io/status'
+url = 'https://intranet.hbtn.io/status'
 response = requests.get(url)
 
 print("Body response:")
-print("    - type:", type(response.text))
-print("    - content:", response.text)
+print("\t- type:", type(response.text))
+print("\t- content:", response.text)
 
