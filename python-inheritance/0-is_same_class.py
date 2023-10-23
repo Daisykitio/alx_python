566b0898249314c78a3bc1fccca87286e7756f3b
Daisykitio <daisycheptoek6788@gmail.com>
2023-10-23 12:38:43 -0700

    0-is_same_class.py

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -1,3 +1,12 @@
+#!/usr/bin/python3
+"""Checks object class"""
+
+
 def is_same_class(obj, a_class):
-    return isinstance(obj, a_class)
+    """Checks object class
+    Args:
+        - obj: object to class
+        - a_class: class to check
+    """
+    return type(obj) is a_class
 
